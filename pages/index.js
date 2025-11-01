1922ec6366437f54396ab41c9e291075872ff848
LuisBacker <53183007+LuisBacker@users.noreply.github.com>
2025-11-01 02:34:31 +0000

    [structure] update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,14 @@
 function Home(){
-    return <><h1>Você é minha inspiração</h1><br></br><img src="https://media.giphy.com/media/v1.Y2lkPWVjZjA1ZTQ3eXlhaDZ3cHc5bzl2eTBxaTN0M2VrOTAwdmxkZXp4dmlxNGJhbnlhbSZlcD12MV9naWZzX3NlYXJjaCZjdD1n/lNyyAveT8jqLLgENJy/giphy.gif"></img></>
+    return (
+    <>
+      <h1>Você é minha inspiração</h1>
+      <br />
+      <img
+        src="https://media.giphy.com/media/v1.Y2lkPWVjZjA1ZTQ3eXlhaDZ3cHc5bzl2eTBxaTN0M2VrOTAwdmxkZXp4dmlxNGJhbnlhbSZlcD12MV9naWZzX3NlYXJjaCZjdD1n/lNyyAveT8jqLLgENJy/giphy.gif"
+        alt="Inspiração"
+      />
+    </>
+  );
 }
 
 export default Home;
