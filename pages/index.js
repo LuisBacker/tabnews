7a6bfc81e36a81e4f1cd9f91b5e0d861acab744d
LuisBacker <53183007+LuisBacker@users.noreply.github.com>
2025-11-01 02:14:06 +0000

    [structure] update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home(){
-    return <h1>Vocêé minha inspiração</h1>
+    return <h1>Voce é minha inspiração</h1> <br></br> <img src=https://media.giphy.com/media/v1.Y2lkPWVjZjA1ZTQ3eXlhaDZ3cHc5bzl2eTBxaTN0M2VrOTAwdmxkZXp4dmlxNGJhbnlhbSZlcD12MV9naWZzX3NlYXJjaCZjdD1n/lNyyAveT8jqLLgENJy/giphy.gi> 
 }
 
 export default Home;
