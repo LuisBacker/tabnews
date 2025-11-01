ae33b94bcf46a08c58455e5da03784d20148cc74
LuisBacker <53183007+LuisBacker@users.noreply.github.com>
2025-11-01 02:42:16 +0000

    [structure] update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 function Home(){
     return (
     <>
-      <h1>Você é minha inspiração</h1>
+      <h1>Você é minha inspiração e minha felicidade</h1>
       <br />
       <img
         src="https://media.giphy.com/media/v1.Y2lkPWVjZjA1ZTQ3eXlhaDZ3cHc5bzl2eTBxaTN0M2VrOTAwdmxkZXp4dmlxNGJhbnlhbSZlcD12MV9naWZzX3NlYXJjaCZjdD1n/lNyyAveT8jqLLgENJy/giphy.gif"
